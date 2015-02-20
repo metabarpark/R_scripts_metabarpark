088891994e14871a106d954c0ea1e1f47afd06af
metabarpark <owenwangensteen@gmail.com>
2015-02-20 21:04:19 +0100

    Create REASSIGN.SAMPLES.AFTER.CROP.R

---
+++ REASSIGN.SAMPLES.AFTER.CROP.R
@@ -0,0 +1,32 @@
+setwd("~/metabarpark_18S")
+filetab <- "MBPARK18S.tab.txt" #Must be a tabulated file (from obitab) including individual sample counts
+fileclust <- "MBPARK18S_1.filtered.noSingleton.len.fasta.cluster.list" #Must be a cluster-list outfile from CROP
+filetag <- "MBPARK18S.crop.tag.tab.txt" #Must be a taxonomy-annotated file from applying ecotag to a cluster.fasta file and then tabulated with obitab
+outfile <- "MBPARK18S.crop.count.tag.csv"
+
+db <- read.table(filetab,sep="\t",head=T)
+db$id <- as.character(db$id)
+db <- db[-c(2,4,5,7,9:12,14,107,108,110:125,127:129)]
+numseqs <- nrow(db)
+clusters <- read.table(fileclust,sep="\t",head=F,stringsAsFactors=F)
+numclust <- nrow(clusters)
+ecotag <- read.table(filetag,sep="\t",head=T,stringsAsFactors=F)
+ecotag <- ecotag[-c(2,5)]
+db.total <- merge(ecotag,db,by="id") #Con esto se queda solo con los heads
+db.total <- data.frame(db.total[-c(ncol(db.total))],cluster_count = 1,db.total$sequence)
+clusters.ord <- data.frame(id=clusters$V1[order(clusters$V1)],included=clusters$V2[order(clusters$V1)])
+rm(clusters)
+for (fila in 1:numclust){
+  head <- clusters.ord[fila,1]
+  tails <- strsplit(as.character(clusters.ord[fila,2]),",")
+  for (j in 1:length(tails[[1]])){
+    tail <- tails[[1]][j]
+    if (tail!=head) { 
+      db.total[fila,c(22,26:116)] <- db.total[fila,c(22,26:116)] + db[db$id==tail,c(3,7:97)] 
+      db.total[fila,119] <- db.total[fila,119]+1
+    } 
+  }
+  print(paste("Cluster", fila, "/",numclust, "ready"))
+}
+write.table(db.total,outfile,sep=";",quote=F,row.names=F)
+print(paste("File ", outfile, " written"))
